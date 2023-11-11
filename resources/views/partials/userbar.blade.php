4af4e6ed62f7b9820d626491b5d071bf9de59986
LilAlamin <ronaldramadhani15@gmail.com>
2023-11-11 18:07:43 +0700

    memperbaiki href pada navbar partial

--- resources/views/partials/userbar.blade.php
+++ resources/views/partials/userbar.blade.php
@@ -63,7 +63,7 @@
     <ul class="menu-inner py-1">
       <!-- Dashboard -->
       <li class="menu-item active">
-        <a href="/" class="menu-link">
+        <a href="/user" class="menu-link">
           <i class="menu-icon tf-icons bx bx-home-circle"></i>
           <div data-i18n="Analytics">Dashboard</div>
         </a>
