cafdbbe215be51b3a737a0da08b706ccdb94754e
LilAlamin <ronaldramadhani15@gmail.com>
2023-11-27 10:11:43 +0700

    memperbaiki href di bagian navbar kour

--- resources/views/partials/kourbar.blade.php
+++ resources/views/partials/kourbar.blade.php
@@ -58,7 +58,7 @@
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
         <li class="menu-item active">
-            <a href="{{ route('kour.index') }}" class="menu-link">
+            <a href="/kour" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Dashboard</div>
             </a>
