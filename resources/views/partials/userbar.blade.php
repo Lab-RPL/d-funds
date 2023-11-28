b20255bbe6f610295afe0b6b82a32092c954efb7
KingNatan <natanaelac04@gmail.com>
2023-11-28 10:59:26 +0700

    Active side bar pada user

--- resources/views/partials/userbar.blade.php
+++ resources/views/partials/userbar.blade.php
@@ -62,7 +62,7 @@
 
   <ul class="menu-inner py-1">
     <!-- Dashboard -->
-    <li class="menu-item {{ Route::currentRouteName() == 'user.index' ? 'active' : '' }}">
+    <li class="menu-item {{ Route::currentRouteName() == 'user.index' || 'user.detail' ? 'active' : '' }}">
       <a href="{{ route('user.index') }}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-home-circle"></i>
         <div data-i18n="Analytics">Dashboard</div>
